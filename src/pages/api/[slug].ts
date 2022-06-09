011fd6402ca1786a91a5196ee34983e327f6c152
qt-coder <chuchenkhang@gmail.com>
2022-06-09 19:47:36 +0800

    Implemented redirecting api

---
+++ src/pages/api/[slug].ts
@@ -0,0 +1,34 @@
+import { prisma } from "../../db/client";
+import { NextApiRequest, NextApiResponse } from "next";
+
+export default async (req: NextApiRequest, res: NextApiResponse) => {
+
+    const slug = req.query['slug']
+
+    if (!slug || typeof slug !== 'string') {
+        res.statusCode = 404
+        res.send(JSON.stringify({ message: 'use a slug' }))
+
+        return
+    }
+
+    
+    const data = await prisma.shortLink.findFirst({
+        where: {
+            slug: {
+                equals: slug
+            }
+        }
+    })
+
+    if (!data) {
+        res.statusCode = 404
+
+        res.send(JSON.stringify({ message: 'slug not found' }))
+
+        return
+    }
+
+    return res.redirect(data.url)
+
+}
